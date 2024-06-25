151012c81cb68fcf3df50c0de7a7d91290cc5251
Tristan <73254843+THG20203@users.noreply.github.com>
2024-06-25 09:32:24 +0100

    sorted the alyout file

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,10 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pixel Positions</title>
+    <!-- reference JavaScript file -->
+    @vite(['resources/js/app.js'])
 </head>
 <body>
     <div>
-        <nav></nav>
+        <nav>
+
+            <div><a href=""><img src="{{ Vite::asset('resources/images/logo.svg') }}" /></a></div>
+
+            <div>links</div>
+            <div>post a job</div>
+        </nav>
         <main>
             {{ $slot }}
         </main>
