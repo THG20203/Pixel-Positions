23f5e8d23288c3cbb04f20efec8ab6dad2a68315
Tristan <73254843+THG20203@users.noreply.github.com>
2024-06-26 11:52:19 +0100

    sorting out some further styling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,23 +1,28 @@
 <x-layout>
     <section>
         <h3>Top Jobs</h3>
-        <div class="p-4 bg-red-500">
-            <div>
-                <div>Laracasts</div>
-                <div>
-                    <h3>Video Producer</h3>
-                    <p>Full Time - From $60,000</p>
-                </div>
+        {{-- flex-col means top to bottom --}}
+        <div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+            {{-- make employer on  the far left with self start --}}
+            <div class="self-start text-sm">Laracasts</div>
 
-                <div>
-                    <div>
-                        <a href="#">Tag</a>
-                        <a href="#">Tag</a>
-                        <a href="#">Tag</a>
-                    </div>
+            <div class="py-8 font-bold">
+                <h3>Video Producer</h3>
+                <p>Full Time - From $60,000</p>
+            </div>
+            {{-- Items-center is used to align flex items along the cross axis in the center.  --}}
 
-                    <img src="https://placehold.co/42x42" alt="" />
+            {{-- Items-center to a container, all the direct children of this container will be centered vertically if the flex direction is row  --}}
+
+            {{-- mt-auto will not make a difference in this case, but in cases where theres more margin to be had, will force the 
+            section below to be at the very bottom of the card --}}
+            <div class="flex justify-between items-center mt-auto">
+                <div>
+                    <a href="#">Tag</a>
+                    <a href="#">Tag</a>
+                    <a href="#">Tag</a>
                 </div>
+                <img src="https://placehold.co/42x42" alt="" />
             </div>
         </div>
     </section>
